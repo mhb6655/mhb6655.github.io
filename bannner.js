b4ac50ecb124112c24942f2b680dcdd0a3f6f89e
mhb6655 <mhb6655@gmail.com>
2022-09-30 21:39:16 +0700

    modified

--- bannner.js
+++ bannner.js
@@ -18,22 +18,41 @@ fetch("banner.json")
 
 
 let ind = 0;
+const tomblRight = document.querySelector('.tampil #banner .right i');
+const tomblLeft = document.querySelector('.tampil #banner .left i');
+tomblLeft.addEventListener('click', function(){
+    })
+    
 
 function ban(){
-    
+    let i;
     let gambarBanner = document.querySelectorAll('.tampil .banner img');
-    for ( let i = 0; i < gambarBanner.length; i++){
+    for ( i = 0; i < gambarBanner.length; i++){
     gambarBanner[i].style.display = "none"
     }
-
-    ind++;
+    let lensBan ="";
+    ind++;    
     if (ind > gambarBanner.length){
         ind = 1
     }
+    for(i = 0; i < gambarBanner.length; i++){
+        lensBan += lens()
+    }
+    document.querySelector('.tampil #banner .len').innerHTML = lensBan;
+    let lensb = document.querySelectorAll('.tampil #banner .len .lens');
+    for(i = 0; i < lensb.length; i++){
+        lensb[i].classList.remove('lens-active')
+    }
+    lensb[ind-1].classList.add('lens-active');
+    lensb[ind-1].style.backgroundColor = 'tomato';
     gambarBanner[ind-1].style.display = "block";
-    setTimeout(ban,4000)
+    setTimeout(ban,6000);
 }
 
 function banner(m){
     return `<img src="${m.gambar}" class='gban'>`
 };
+
+function lens(){
+    return `<span class='lens'></span>`
+}
