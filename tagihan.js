3cfc5df84736899a11de4b4e0b29c5d79aed9372
mhb6655 <mhb6655@gmail.com>
2022-10-01 00:33:31 +0700

    mengubah dan menambah baru

---
+++ tagihan.js
@@ -0,0 +1,29 @@
+fetch('tagihan.json')
+
+.then(m => m.json())
+.then(m =>{
+    let card = "";
+    m.forEach(e => {
+        card += tem(e)
+    });
+    document.querySelector('.table #leng .leng').innerHTML = card;
+    let list = document.querySelectorAll('.table #leng .leng h4');
+    list.forEach(m =>{
+        m.addEventListener('mouseenter', function(){
+            m.classList.add('h-active')
+        })
+        m.addEventListener('mouseleave', function(){
+            m.classList.remove('h-active')
+        })
+    })
+})
+.catch((err) => {
+    
+});
+
+
+
+
+function tem(m){
+    return `<h4>${m.nama}</h4>`
+}
